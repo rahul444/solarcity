063870268f0cf17b67457807a23c69a5299789a7
Rahul <rahul444@hotmail.com>
2017-03-13 00:06:02 -0700

    basic impl working

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-var path = require('path');
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
 var port = process.env.PORT || 3000;
+var db;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, function() {
     console.log('listening on port: ' + port)
 });
 
+MongoClient.connect('mongodb://solarcity:solarCity1@ds129260.mlab.com:29260/solar-city', (err, database) => {
+    if (err) return console.log(err);
+    db = database;
+    app.listen(4000, () => {
+        console.log('db listening on 4000');
+    });
+});
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    var recents = db.collection('userinfo').find().sort({_id:1}).limit(50);
+    console.log(recents);
+});
+
+/**
+ * Post user info, which includes name, age, address, and reason for interest, to the DB.
+ */
+app.post('/submitUserInfo', function(req, res) {
+    var name = req.body.name;
+    var age = req.body.age;
+    var address = req.body.address;
+    var interest = req.body.interest;
+
+    db.collection('userinfo').insert({"_id" : name + '_' + address, [name] : {"age":age, "address":address, "interest":interest}});
+
+    console.log(req.body);
+    res.redirect("/");
+});
+
+/**
+ * Get user info from the DB using the user's name and address.
+ */
+app.get('/getUserInfo', function(req, res) {
+    var name = req.query.name;
+    var address = req.query.address;
+
+    db.collection('userinfo').findOne({"_id" : name + '_' + address}, function(err, doc) {
+        if (doc) {
+            res.send(doc);
+        } else if (err) {
+            console.log(err);
+        }
+    });
 });
